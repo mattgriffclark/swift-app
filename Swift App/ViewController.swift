c3750231d8fc54ea22bebfe0f67b6e3233107be9
Matt Clark <mattclark@davidm-pc.fescony.local>
2017-02-09 16:41:38 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Matt Clark on 2/9/17.
+//  Copyright © 2017 Matt Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
